ff9018c511a76e598b2b96ae127eb5ffcd757f90
Caíque Villela <92598305+analistacaique@users.noreply.github.com>
2022-04-16 19:08:54 -0300

    Add files via upload

---
+++ Temperatura.java
@@ -0,0 +1,12 @@
+package Fundamentos;
+
+public class Temperatura {
+
+	public static void main(String[] args) {
+		double FATOR = 5.0 / 9.0; 
+		double AJUSTE = 32;
+		double fahrenheit = 86;
+		double celsius = (fahrenheit - AJUSTE) * FATOR; 
+				System.out.println("O resultado é " + celsius + "°C.");
+	}
+}
